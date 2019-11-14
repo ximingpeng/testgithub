fa0cf6ce18148ba4664cf60e35e141c884552f7c
ximingpeng <1603422829@qq.com>
2019-11-14 18:16:40 +0800

    增加dem方法

--- testgithub/src/com/xmp/demo1.java
+++ testgithub/src/com/xmp/demo1.java
@@ -4,4 +4,7 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("这是demo1的防范发");
     }
+    public void  demo(){
+        System.out.println("这是demo");
+    }
 }
